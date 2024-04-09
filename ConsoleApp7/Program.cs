b5355a56b86b92f1d739af112e2fdd7bc65f8ae2
Amel Rahmanovic <amelrahmanovic90@gmail.com>
2024-04-09 13:56:57 +0200

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,69 @@
+﻿using RabbitMQHelper;
+using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RabbitMQCRUD rabbitMQHelper = new RabbitMQCRUD("localhost", "rabbitmq", "rabbitmq");
+
+            int opcija=1;
+            do
+            {
+                bool succesInput;
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("0-Add:");
+                        Console.WriteLine("1-Read + Ack:");
+                        Console.WriteLine("2-Read:");
+                        Console.WriteLine("3-Check Queue:");
+                        Console.WriteLine("4-Check Queue number Messages:");
+                        Console.WriteLine("5-Delete Queue:");
+                        Console.WriteLine("10-EXIT:");
+                        Console.WriteLine("Option:");
+                        opcija = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                        succesInput = true;
+                    }
+                    catch (Exception)
+                    {
+                        succesInput = false;
+                        Console.Clear();
+                        Console.WriteLine("This is not number...");
+                    }
+                } while (!succesInput);
+                
+
+                switch (opcija)
+                {
+                    case 0:
+                        Random random = new Random();
+                        rabbitMQHelper.NewQueues("test", "test" + random.Next().ToString());
+                        break;
+                    case 1:
+                        Console.WriteLine(rabbitMQHelper.GetQueues("test", false, true));
+                        break;
+                    case 2:
+                        Console.WriteLine(rabbitMQHelper.GetQueues("test", false, false));
+                        break;
+                    case 3: 
+                        Console.WriteLine("Queue: "+rabbitMQHelper.ExistQueue("test"));
+                        break;
+                    case 4:
+                        Console.WriteLine("Messages:"+rabbitMQHelper.CountQueuesMessages("test"));
+                        break;
+                    case 5:
+                        rabbitMQHelper.DeleteQueue("test");
+                        break;
+
+                    case 10: Environment.Exit(0); break;
+                    default: break;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ ConsoleApp7/RabbitMQCRUD.cs
@@ -0,0 +1,148 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading.Channels;
+
+namespace RabbitMQHelper
+{
+    public class RabbitMQCRUD
+    {
+        private ConnectionFactory factory;
+        private IConnection connection;
+        private string HostName, UserName, Password;
+
+        public RabbitMQCRUD(string HostName, string UserName, string Password)
+        {
+            this.HostName = HostName;
+            this.UserName = UserName;
+            this.Password = Password;
+            //OpenConnection();
+        }
+
+        private void OpenConnection()
+        {
+            factory = new ConnectionFactory()
+            {
+                //HostName = HostName, // Docker host
+                UserName = UserName, // RabbitMQ username
+                Password = Password // RabbitMQ password
+            };
+            try//try to connect from local machine
+            {
+                factory.HostName = HostName;
+                connection = factory.CreateConnection();
+            }
+            catch (Exception)//connect to docker
+            {
+                factory.HostName = "rabbitmq";
+                connection = factory.CreateConnection();
+            }
+        }
+
+        public void NewQueues(string nameQueue, string json)
+        {
+            OpenConnection();
+            var channel = connection.CreateModel();
+
+            //channel.QueueDelete(queue: nameQueue);
+
+            channel.QueueDeclare(queue: nameQueue,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: nameQueue,
+                                 basicProperties: null,
+                                 body: Encoding.UTF8.GetBytes(json));
+            channel.Close();
+        }
+        public int CountQueuesMessages(string nameQueues)
+        {
+            OpenConnection();
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: nameQueues,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var result = channel.QueueDeclarePassive(nameQueues);
+                
+                connection.Close();
+
+                return (int)result.MessageCount;
+            }
+        }
+        public void DeleteQueue(string nameQueue)
+        {
+            OpenConnection();
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDelete(queue: nameQueue);
+            }
+            connection.Close();
+        }
+        public string GetQueues(string nameQueue, bool autoAck, bool AckMessage)
+        {
+            string message = "";
+            try
+            {
+                OpenConnection();
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "users",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    // Create a consumer
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        message = Encoding.UTF8.GetString(body.ToArray());
+                        try
+                        {
+                            if (AckMessage)
+                                channel.BasicAck(ea.DeliveryTag, false);
+
+                        }
+                        catch (Exception) { }
+                    };
+
+                    // Start consuming messages
+                    string consumerTag = channel.BasicConsume(queue: nameQueue,
+                                         autoAck: autoAck,
+                                         consumer: consumer);
+                }
+                connection.Close();
+            }
+            catch (Exception){}
+            return message;
+        }
+        public bool ExistQueue(string nameQueue)
+        {
+            OpenConnection();
+            using (var channel = connection.CreateModel())
+            {
+                try
+                {
+                    channel.QueueDeclarePassive(nameQueue);
+                    connection.Close();
+                    return true;
+                }
+                catch (RabbitMQ.Client.Exceptions.OperationInterruptedException)
+                {
+                    connection.Close();
+                    return false;
+                }
+            }
+            
+        }
+    }
+}
